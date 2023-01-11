8db80fc4e6c27db69956271d850ab84c38b98412
medhousni <119140191+medhousni@users.noreply.github.com>
2023-01-11 23:03:53 +0100

    Add files via upload

---
+++ myproject/api/views.py
@@ -0,0 +1,17 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from base.models import Item
+from .serializers import ItemSerializer
+
+@api_view(['GET'])
+def getData(request):
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True) #True tells the serializer that we are going to serialize multiple items
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addItem(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
